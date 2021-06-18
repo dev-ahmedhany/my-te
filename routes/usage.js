74ca11c55c8cc562f36eb0ad7b274442de5d4c4b
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2021-06-18 11:08:14 +0200

    fix bug

--- routes/usage.js
+++ routes/usage.js
@@ -8,7 +8,7 @@ const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 
-const browser;
+let browser;
 (() => {
   browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
